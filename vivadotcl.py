5b7dc7b360b5dea707600d6fa193ebd1bde57918
Iru Cai <mytbk920423@gmail.com>
2017-07-25 16:02:50 +0800

    fix vivado prompt, add demo test

--- vivadotcl.py
+++ vivadotcl.py
@@ -2,16 +2,18 @@
 
 import pexpect
 
+VIVADO_PROMPT = '[Vv]ivado.*% '
+
 class VivadoInstance():
 	def __init__(self, path):
 		self.vivado = pexpect.spawn(path, ['-nojournal', '-nolog', '-mode', 'tcl'])
-		self.vivado.expect('% ')
+		self.vivado.expect(VIVADO_PROMPT)
 		self.hw_opening = False
 
 	def send_command(self, s):
 		self.vivado.sendline(s)
 		self.vivado.expect('\r\n')
-		self.vivado.expect('vivado.*% ')
+		self.vivado.expect(VIVADO_PROMPT)
 		return self.vivado.before
 
 	def source_tcl(self, tclfile):
@@ -36,3 +38,16 @@ class VivadoInstance():
 		self.hw_opening = True
 		self.send_command('set_property PROGRAM.FILE { ' + bitfile + '} [current_hw_device]')
 		return self.send_command('program_hw_devices [current_hw_device]')
+
+if __name__ == "__main__":
+    import sys
+    # example: python2 vivadotcl.py \
+    # /usr/local/Vivado_Lab/2017.2/bin/vivado_lab test.bit
+    # Tested with Vivado WebPACK 2017.2 and Vivado Lab Edition 2017.2
+    vivado_path = sys.argv[1]
+    bit_path = sys.argv[2]
+    v = VivadoInstance(vivado_path)
+    v.connect_hw_server()
+    hw = v.get_hw_targets()
+    print 'all hardware targets: ' + str(hw)
+    print v.program_target(hw[0], bit_path)
