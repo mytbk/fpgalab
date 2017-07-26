3866b1d9687ece97120dc03a4201c0b723cfdbb1
Iru Cai <mytbk920423@gmail.com>
2017-07-26 15:14:18 +0800

    small fix, autopep8

--- vivadotcl.py
+++ vivadotcl.py
@@ -4,40 +4,45 @@ import pexpect
 
 VIVADO_PROMPT = '[Vv]ivado.*% '
 
+
 class VivadoInstance():
-	def __init__(self, path):
-		self.vivado = pexpect.spawn(path, ['-nojournal', '-nolog', '-mode', 'tcl'])
-		self.vivado.expect(VIVADO_PROMPT)
-		self.hw_opening = False
 
-	def send_command(self, s):
-		self.vivado.sendline(s)
-		self.vivado.expect('\r\n')
-		self.vivado.expect(VIVADO_PROMPT)
-		return self.vivado.before
+    def __init__(self, path):
+        self.vivado = pexpect.spawn(
+            path, ['-nojournal', '-nolog', '-mode', 'tcl'])
+        self.vivado.expect(VIVADO_PROMPT)
+        self.hw_opening = None
 
-	def source_tcl(self, tclfile):
-		result = self.send_command('source ' + tclfile)
-		return result
+    def send_command(self, s):
+        self.vivado.sendline(s)
+        self.vivado.expect('\r\n')
+        self.vivado.expect(VIVADO_PROMPT)
+        return self.vivado.before
 
-	def connect_hw_server(self):
-		self.send_command('open_hw')
-		self.send_command('connect_hw_server')
+    def source_tcl(self, tclfile):
+        result = self.send_command('source ' + tclfile)
+        return result
 
-	def get_hw_targets(self):
-		self.send_command('refresh_hw_server')
-		hws = self.send_command('puts [get_hw_targets -quiet]')
-		return hws.split()
+    def connect_hw_server(self):
+        self.send_command('open_hw')
+        self.send_command('connect_hw_server')
 
-	def program_target(self, target, bitfile):
-		if self.hw_opening:
-			self.send_command('close_hw_target')
-			self.hw_opening = False
+    def get_hw_targets(self):
+        self.send_command('refresh_hw_server')
+        hws = self.send_command('puts [get_hw_targets -quiet]')
+        return hws.split()
 
-		self.send_command('open_hw_target ' + target)
-		self.hw_opening = True
-		self.send_command('set_property PROGRAM.FILE { ' + bitfile + '} [current_hw_device]')
-		return self.send_command('program_hw_devices [current_hw_device]')
+    def program_target(self, target, bitfile):
+        if self.hw_opening is not None:
+            if self.hw_opening != target:
+                self.send_command('close_hw_target')
+                self.send_command('open_hw_target ' + target)
+        else:
+            self.send_command('open_hw_target ' + target)
+
+        self.send_command(
+            'set_property PROGRAM.FILE { ' + bitfile + '} [current_hw_device]')
+        return self.send_command('program_hw_devices [current_hw_device]')
 
 if __name__ == "__main__":
     import sys
